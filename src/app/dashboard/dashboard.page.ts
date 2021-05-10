0218d2ac08af2a72d5a27213e75385aabd917b4e
MaelFournier <mfournier2@la-joliverie.com>
2021-05-10 11:04:20 +0200

    retrieve data from an user

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -2,36 +2,57 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { AuthenticateService } from '../services/authentication.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
 })
+
 export class DashboardPage implements OnInit {
 
   userEmail: string;
+  user:{
+    uid: string,
+    email: string
+  }
+  data:any
 
   constructor(
     private navCtrl: NavController,
-    private authService: AuthenticateService
-  ) { }
+    private authService: AuthenticateService,
+    private db: AngularFirestore,
+  ) {
+   }
 
+  
   ngOnInit() {
-
+    //Permet de vérifier si le user est bien connecté, sinon il est renvoyé page login
     this.authService.userDetails().subscribe(res => {
-      console.log('res', res);
       if (res !== null) {
         this.userEmail = res.email;
+        this.getuser(this.userEmail).subscribe((data)=>{
+          this.data = data;
+          console.log(this.data[0])
+          var user = {
+            uid: this.data[0].UID,
+            email: this.data[0].email
+          }
+          console.log(user);
+      });
+
       } else {
         this.navCtrl.navigateBack('');
       }
     }, err => {
       console.log('err', err);
-    })
-
+    });
   }
-
+  
   logout() {
     this.authService.logoutUser()
       .then(res => {
@@ -42,4 +63,10 @@ export class DashboardPage implements OnInit {
         console.log(error);
       })
   }
+
+  //Permet de réupérer le user associé à l'adresse email rentrée précèdemment
+  getuser(userEmail: string):Observable<any>{
+    return this.db.collection<any>("users", ref => ref.where('email','==',this.userEmail)).valueChanges();
+  }
 }
+
